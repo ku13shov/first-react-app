998c88459a4eeff1e73ac2a0f10410e57791510c
Пользователь <kuleshov000@gmail.com>
2023-04-15 16:05:57 +0300

    first fix

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,18 +2,18 @@ import { useState, useRef } from 'react';
 import styles from './Form.module.css';
 
 function Form() {
+  const [inputColor, setInputColor] = useState('');
   const inputRef = useRef(null);
-  const [state, setState] = useState('');
 
-  const formSubmitHandler = function(e) {
+  const formSubmitHandler = (e) => {
     e.preventDefault();
-    setState(inputRef.current.value);
+    setInputColor(inputRef.current.value);
   }
 
   return (
     <form className={styles.form} onSubmit={formSubmitHandler}>
       <label className={styles.label} htmlFor="color">Enter color name</label>
-      <input className={styles.input} type="text" name="color" style={{backgroundColor: state}} ref={inputRef} placeholder='orange' />
+      <input className={styles.input} type="text" name="color" style={{backgroundColor: inputColor}} ref={inputRef} placeholder='orange' />
       <button className={styles.button} type="submit">Submit</button>
     </form>
   );
