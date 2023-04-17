7e302c21f6b60d8933d4c9237ffe864bfc135880
ku13shov <kuleshov000@gmail.com>
2023-04-17 14:18:21 +0300

    add classNames

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,22 @@
 import { useState, useRef } from 'react';
-import styles from './Form.module.css';
+import classNames from 'classnames';
+import './Form.css';
 
-function Form() {
+const Form = () => {
   const [inputColor, setInputColor] = useState('');
   const inputRef = useRef(null);
+
+  const classNamesArray = ['input'];
+
+  if (inputColor === 'red') {
+    classNamesArray.push('red');
+  }
+
+  if (inputColor === 'orange') {
+    classNamesArray.push('orange');
+  }
+
+  const inputClasses = classNames(classNamesArray);
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
@@ -11,10 +24,11 @@ function Form() {
   }
 
   return (
-    <form className={styles.form} onSubmit={formSubmitHandler}>
-      <label className={styles.label} htmlFor="color">Enter color name</label>
-      <input className={styles.input} type="text" name="color" style={{backgroundColor: inputColor}} ref={inputRef} placeholder='orange' />
-      <button className={styles.button} type="submit">Submit</button>
+    <form className='form' onSubmit={formSubmitHandler}>
+      <label className='label' htmlFor="color">Enter color name</label>
+      {/* <input className={styles.input} type="text" name="color" style={{backgroundColor: inputColor}} ref={inputRef} placeholder='orange' /> */}
+      <input className={inputClasses} type="text" name="color" ref={inputRef} placeholder='orange' />
+      <button className='button' type="submit">Submit</button>
     </form>
   );
 }
